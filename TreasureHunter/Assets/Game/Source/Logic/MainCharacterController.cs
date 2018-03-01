affc1c8605e63f98cac80345ea20f0d07662637e
srgvn <srajngaming@gmail.com>
2018-03-01 14:54:01 +0700

    Update

---
+++ TreasureHunter/Assets/Game/Source/Logic/MainCharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterController : MonoBehaviour {
+
+	public int skinActive;
+	private static MainCharacterController _mainCtrl;
+	public static MainCharacterController MainCtrl {
+		get { 
+			return _mainCtrl;
+		}
+	}
+
+	void Awake() {
+		if (_mainCtrl != null) {
+			Destroy (this.gameObject);
+		}
+		_mainCtrl = this;
+		LoadAnimation (skinActive);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LoadAnimation(int skinActive) {
+		if (skinActive == 1) {
+			this.gameObject.transform.GetChild(0).GetComponent<Animator>().
+		}
+	}
+}
